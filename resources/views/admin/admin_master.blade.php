dfb7d804add68a6f1db26d5b05aabeb4e5acf608
YudiGF <yudigf08@gmail.com>
2023-09-18 16:25:11 +0700

    add tinymce admin_master

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -119,8 +119,15 @@
                 toastr.error(" {{ Session::get('message') }} ");
                 break; 
             }
-            @endif 
+            @endif
         </script>
+
+        <!--tinymce js-->
+        <script src="{{ asset('backend/assets/libs/tinymce/tinymce.min.js') }}"></script>
+
+        <!-- init js -->
+        <script src="{{ asset('backend/assets/js/pages/form-editor.init.js') }}"></script>
+
     </body>
 
 </html>
