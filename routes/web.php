2abd70addc2646d3b2e54a2b5701e5447ad44299
YudiGF <yudigf08@gmail.com>
2023-09-20 14:35:02 +0700

    add route multiimage

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,11 @@ Route::controller(AboutController::class)->group(function () {
     Route::get('/about/page', 'aboutPage')->name('about.page');
     Route::post('/update/about', 'updateAbout')->name('update.about');
     Route::get('/about', 'homeAbout')->name('home.about');
+    Route::get('/about/multi/image', 'aboutMultiImage')->name('about.multi.image');
+    Route::post('/store/multi/image', 'storeMultiImage')->name('store.multi.image');
+    Route::get('/all/multi/image', 'allMultiImage')->name('all.multi.image');
+    Route::get('/edit/multi/image/{id}', 'editMultiImage')->name('edit.multi.image');
+    Route::post('/update/multi/image', 'updateMultiImage')->name('update.multi.image');
     
 });
 
