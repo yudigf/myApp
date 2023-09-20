2a960597dcaca02b175729c3cbff5d3a22ae40fb
YudiGF <yudigf08@gmail.com>
2023-09-20 08:12:25 +0700

    add route about, home about

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::controller(HomeSliderController::class)->group(function () {
 Route::controller(AboutController::class)->group(function () {
     Route::get('/about/page', 'aboutPage')->name('about.page');
     Route::post('/update/about', 'updateAbout')->name('update.about');
+    Route::get('/about', 'homeAbout')->name('home.about');
     
 });
 
